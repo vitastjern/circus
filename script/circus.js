cde27f6ae483bd7b2ac5e1c9fd8d2f067649e818
ygberg <fredrik.ygberg@gmail.com>
2021-05-04 17:04:30 +0200

    nav bar and adfader

--- script/circus.js
+++ script/circus.js
@@ -1,26 +1,43 @@
 $(document).ready(function(){
-    // Run our swapImages() function every 5secs
-    setInterval('swapImages()', 5000);
-  });
+   
+    $( window ).scroll(function(){stickyNavbar()}); //runs the stickynavbar function when scrolling main window
+    $(this).scrollTop(0); // scrolls the page back to top on reload 
+    setInterval('swapImages()', 5000); // Runs the  swapImages() function every 5secs
+    $('#fadeGallery').click(function(){showShows()});
+   
+});
 
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+//function stickyNavbar() {
+   
+//var stickybar = ($('#navbar').offset().top >= window.pageYOffset) ? $('#navbar').addClass("navstick") : $('#navbar').removeClass("navstick") ;
+//$('#fadeGallery').css('padding-top', '55');
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+//}
+ 
+var stickybar = $('#navbar').offset(); 
+
+function stickyNavbar() {
+    $('#showlist').css('padding-top', '55');
+    $('#fadeGallery').css('padding-top', '55');
+    if (window.pageYOffset >= stickybar.top) {
+        $('#navbar').addClass("navstick");
+    } else {
+        $('#navbar').removeClass("navstick");
+    }
+    
+} 
+
+function showShows(){
+$('#showtext').css('hight', '400');
+$('#showtext').css('with', '400');
+$('#showtext').append('test of the new div ')
 }
 
-
-function swapImages(){
-    var $active = $('#myGallery .active');
-    var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
-    $active.fadeOut(function(){
+function swapImages(){                              // ? :  is like a if else
+    var $active = $('#fadeGallery .active');          
+    var $next = ($('#fadeGallery .active').next().length > 0) ? $('#fadeGallery .active').next() : $('#fadeGallery img:first');
+    $active.fadeOut(function(){         //function that on fadeOut changes the active image by remove and adding the active class
     $active.removeClass('active');
     $next.fadeIn().addClass('active');
     });
--- script/order.js
+++ script/order.js
@@ -1,1 +1,17 @@
-// my version with fixed decodeURI
+$(document).ready(function(){
+   
+    $( window ).scroll(function(){stickyNavbar()}); //runs the stickynavbar function when scrolling main window
+    $(this).scrollTop(0); // scrolls the page back to top on reload 
+   
+});
+
+var stickybar = $('#navbar').offset();
+
+function stickyNavbar() {
+    $('#myGallery').css('padding-top', '55');
+    if (window.pageYOffset >= stickybar.top) {
+        $('#navbar').addClass("navstick");
+    } else {
+        $('#navbar').removeClass("navstick");
+    }
+}
