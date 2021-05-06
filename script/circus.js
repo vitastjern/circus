4ed279de41dccbc386de57db665f07b404e62648
ygberg <fredrik.ygberg@gmail.com>
2021-05-06 10:58:39 +0200

    Fixed bugs on the main page

--- script/circus.js
+++ script/circus.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-   
-    $(this).scrollTop(0);                               // scrolls the page back to top on reload 
-    $( window ).scroll(function(){stickyNavbar()});     //runs the stickynavbar function when scrolling main window
+    stickyNavbar();
+   //$(this).scrollTop(0);                               // scrolls the page back to top on reload 
+    //$( window ).scroll(function(){stickyNavbar()});     //runs the stickynavbar function when scrolling main window
     setInterval('swapImages()', 5000);                  // Runs the  swapImages() function every 5secs
     $('#fadeGallery').click(function(){showShows()});
     $( document ).tooltip();
@@ -10,20 +10,18 @@ $(document).ready(function(){
 
 
 function stickyNavbar() {
-var stickybar = ($('#navbar').offset().top >= window.pageYOffset) ? $('#navbar').addClass("navstick") : $('#navbar').removeClass("navstick") ;
+    //$('#fadeGallery').css ( 'padding-top', '70px'); // this was use to counter a bug not needed anymore
+    var stickybar = ($('#navbar').offset().top <= window.pageYOffset) ? $('#navbar').addClass("navstick") : $('#navbar').removeClass("navstick") ;
 }
  
-/* var stickybar = $('#navbar').offset(); 
-
+/* var stickybar = $('#navbar').offset();  // this is the old code for the function above keept it in since it much easier to read
 function stickyNavbar() {
-    $('#showlist').css('padding-top', '55');
     $('#fadeGallery').css('padding-top', '55');
     if (window.pageYOffset >= stickybar.top) {
         $('#navbar').addClass("navstick");
     } else {
         $('#navbar').removeClass("navstick");
     }
-    
 }  */
 
 function showShows(){
