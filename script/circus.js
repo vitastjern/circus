85525b3b7a5e8b6948074daa1f3669c1bb8f220f
ygberg <fredrik.ygberg@gmail.com>
2021-05-10 19:24:51 +0200

    Latest fixes

--- script/circus.js
+++ script/circus.js
@@ -1,19 +1,14 @@
 $(document).ready(function(){
     stickyNavbar();
-    //$(this).scrollTop(0);                               // scrolls the page back to top on reload 
-    //$( window ).scroll(function(){stickyNavbar()});     //runs the stickynavbar function when scrolling main window
     setInterval('swapImages()', 5000);                  // Runs the  swapImages() function every 5secs
     $('#fadeGallery').click(function(){window.location="#bookinganchor"});
-    //$( document ).tooltip();
+    $('#showlist').click(function(){window.location="#bookinganchor"});
     $(".about_button").click(function(){
         $('#about').css('height', '400');
         $('#about').css('width', '400');
-       
         $('#about').toggle(1000);
         $('#about-ida').toggle(1000);
         $('#aboutimg2').toggle(1000).css('display:none');
-        
-       
     });
    
 });
@@ -23,25 +18,6 @@ function stickyNavbar() {
 var stickybar = ($('#navbar').offset().top <= window.pageYOffset) ? $('#navbar').addClass("navstick") : $('#navbar').removeClass("navstick") ;
 }
  
-/* var stickybar = $('#navbar').offset();  // this is the old code for the function above kept it in since it much easier to read
-function stickyNavbar() {
-    $('#fadeGallery').css('padding-top', '55');
-    if (window.pageYOffset >= stickybar.top) {
-        $('#navbar').addClass("navstick");
-    } else {
-        $('#navbar').removeClass("navstick");
-    }
-}  */
-
-
-/* 
-
-function showShows(){
-    $('#showtext').css('height', '400');
-    $('#showtext').css('width', '400');
-    $('#showtext').html('test of the new div ').toggle();
-} */
-
 function swapImages(){                              // ? :  is like a if else
     var $active = $('#fadeGallery .active');          
     var $next = ($('#fadeGallery .active').next().length > 0) ? $('#fadeGallery .active').next() : $('#fadeGallery img:first');
@@ -50,18 +26,3 @@ function swapImages(){                              // ? :  is like a if else
     $next.fadeIn().addClass('active');
     });
   }
-
-
-/* // attachEvents() makes the about_button image clickable, should probably make it a bit more general..
-// calls the clicked() function which displays/hides the text
-/* function attachEvents() {  
-    $(".about_button").click(clicked);
-    $("#aboutimg").click(clicked);
-}
-    
-function clicked() {
-    $('#about').css('height', '400');
-    $('#about').css('width', '400');
-    $('#about').toggle(1000);
-}
- */
